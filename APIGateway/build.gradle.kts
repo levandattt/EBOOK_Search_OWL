e4634de2dff21edf789376b05aecc4a5fd5fc007
tqchu <102200370@sv1.dut.udn.vn>
2024-11-05 09:37:18 +0700

    update apigateway

--- APIGateway/build.gradle.kts
+++ APIGateway/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     java
     id("io.spring.dependency-management") version "1.1.6"
+    id("org.springframework.boot") version "2.7.16"
 }
 
-extra["springCloudVersion"] = "2023.0.3"
+extra["springCloudVersion"] = "2021.0.8"
 
 // Common repositories across all projects
 repositories {
@@ -22,21 +23,11 @@ repositories {
 }
 
 dependencies {
-    // Spring Cloud Gateway for WebFlux
-    // https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-starter-gateway
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
-
-    // SpringDoc OpenAPI for WebFlux
-    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.1.0")
-
-    // Spring Boot WebFlux
+    implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.14") // Compatible version for Spring Boot 2.7.x
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-
-    // Ensure Reactor Netty 1.0.x for Spring Boot 2.7.x
     implementation("io.projectreactor.netty:reactor-netty-http:1.0.34") // Latest 1.0.x version
     implementation("io.projectreactor.netty:reactor-netty-core:1.0.34")
-
-
 }
 
 
